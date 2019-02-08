5e5d49133d6f4180347a30d030de2657ea673522
wwolny <wwolny1@gmail.com>
2019-02-08 15:31:17 +0100

    menace.py check_box()

--- menace.py
+++ menace.py
@@ -7,23 +7,29 @@ from box import Box
 
 class Menace:
     # Array of boxes
-    boxes = [Box(1, board=[0, 0, 0, 0, 0, 1, 2, 2, 1])]
+    boxes = {'000000000': Box(0, board=[0, 0, 0, 0, 0, 0, 0, 0, 0])}
 
     # array of chosen boxes
-    chosen = [-1]*4
+    chosen = ['000000000', '', '', '']
 
     def __init__(self):
         print("I'm the machine learning")
-        self.initBoxes()
-
-    def init_boxes(self):
-        print("I initialize boxes")
 
     # returns the position where MENACE wants to move
     def move(self, board):
         print("I make a move")
         position = 0
         return position
+
+    # return:
+    # True - the box already exists
+    # False - the box doesn't exist
+    def check_box(self, board):
+        str1 = ''
+        for i in range(0, 9):
+            str1 += board[i]
+        return str in self.boxes
+
 
     # result =>
     # loser = 0
@@ -33,5 +39,11 @@ class Menace:
         for i in range(0, 5):
             self.boxes[self.chosen[i]].result(result)
 
-
-
+    # rotations = [(0, 1, 2, 3, 4, 5, 6, 7, 8),
+    #              (2, 1, 0, 5, 4, 3, 8, 7, 6),
+    #              (6, 7, 8, 3, 4, 5, 0, 1, 2),
+    #              (6, 3, 0, 7, 4, 1, 8, 5, 2),
+    #              (8, 7, 6, 5, 4, 3, 2, 1, 0),
+    #              (2, 5, 8, 1, 4, 7, 0, 3, 6),
+    #              (0, 3, 6, 1, 4, 7, 2, 5, 8),
+    #              (8, 5, 2, 7, 4, 1, 6, 3, 0)]
